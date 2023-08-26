20d52dde3e89885dd5b8e46c4384fa85a2f8a996
Abeladmassu16 <abeladmassu3097@gmail.com>
2023-08-26 09:12:26 +0300

    edited

--- my.js
+++ my.js
@@ -1,34 +1,8 @@
-document.getElementById("emailForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-
-    const emailData = {
-        to: "abeladmassu3097@gmail.com",
-        subject: "New Contact Form Submission",
-        body: `
-            Name: ${formData.get("Name")}
-            Email: ${formData.get("Email")}
-            Message: ${formData.get("Message")}
-        `
-    };
-
-    fetch("/send-email", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(emailData)
-    })
-    .then(response => {
-        if (response.ok) {
-            alert("Email sent successfully!");
-        } else {
-            alert("Email sending failed.");
-        }
-    })
-    .catch(error => {
-        console.error("Error sending email:", error);
-    });
+// JavaScript for handling the mobile navigation menu
+document.querySelector('.fas.fa-bars').addEventListener('click', function () {
+    document.querySelector('nav ul').classList.toggle('show');
 });
 
+document.querySelector('.fas.fa-times').addEventListener('click', function () {
+    document.querySelector('nav ul').classList.remove('show');
+});
